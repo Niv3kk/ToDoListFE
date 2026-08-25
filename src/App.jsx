1b849e586ed86725e49ea40e421a253cb5d2f80c
Niv3k <condorinicolaskevin@gmail.com>
2026-08-25 18:48:30 -0400

    TD-6:IMPORTAR GETALL() EN APP.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { useEffect } from 'react';
+import { getAll } from './services/task.service';
 
 function App() {
+  useEffect(() => {
+    const obtenerTareas = async () => {
+      try {
+        const response = await getAll();
+
+        console.log('Respuesta de tareas:', response);
+      } catch (error) {
+        console.error('Error al obtener las tareas:', error);
+      }
+    };
+
+    obtenerTareas();
+  }, []);
 
   return (
     <>
-      <h1>Hello word</h1>
+      <h1>Hola Mundo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
